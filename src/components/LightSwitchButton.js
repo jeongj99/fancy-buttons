888d3d649d201a68ae2cca6f52074b4d0e5a9819
Jae Yun Jeong <jeongalex0616@gmail.com>
2022-10-31 16:24:36 -0300

    Set LightSwitchButton useState default to 'off'

--- src/components/LightSwitchButton.js
+++ src/components/LightSwitchButton.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 function LightSwitchButton() {
-  const [light, setLight] = useState();
+  const [light, setLight] = useState('off');
 
   return (
     <button className="LightSwitchButton">
