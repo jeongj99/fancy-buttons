93abfa1b44c56dd39b46f40a778f13b2933f31eb
Jae Yun Jeong <jeongalex0616@gmail.com>
2022-10-31 15:21:06 -0300

    Create components folder and the components files for buttons

---
+++ src/components/LightSwitchButton.js
@@ -0,0 +1,10 @@
+function LightSwitchButton() {
+  return (
+    <button className="LightSwitchButton">
+      <span className="On"><i>💡</i> I'm on!</span>
+      <span className="Off"><i>💡</i> I'm off!</span>
+    </button>
+  );
+}
+
+export default LightSwitchButton;
