a19dc85d482b460845eed04a8a273cbbdcf11569
Jae Yun Jeong <jeongalex0616@gmail.com>
2022-10-31 16:45:53 -0300

    Add onClick and a handleClick for the TextRepeaterButton

--- src/components/TextRepeaterButton.js
+++ src/components/TextRepeaterButton.js
@@ -5,11 +5,13 @@ function TextRepeaterButton() {
 
   const textArray = [];
   for (let i = 0; i < repetitions; i++) {
-    textArray.push(<span key={i}>I like this text</span>);
+    textArray.push(<span key={i}>I like this text </span>);
   }
 
+  const handleClick = () => setRepetitions(repetitions + 1);
+
   return (
-    <button className="TextRepeaterButton">
+    <button onClick={handleClick} className="TextRepeaterButton">
       {textArray}
     </button>
   );
