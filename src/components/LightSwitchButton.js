7b6e7573c5144de77ded212c8f6fe09de1001db6
Jae Yun Jeong <jeongalex0616@gmail.com>
2022-10-31 16:44:06 -0300

    Add onClick and a handleClick for the LightSwitchButton

--- src/components/LightSwitchButton.js
+++ src/components/LightSwitchButton.js
@@ -3,10 +3,18 @@ import { useState } from "react";
 function LightSwitchButton() {
   const [light, setLight] = useState('off');
 
+  const handleClick = () => {
+    if (light === 'on') {
+      setLight('off');
+    } else {
+      setLight('on');
+    }
+  };
+
   return (
-    <button className="LightSwitchButton">
-      {light === "on" && <span className="On"><i>💡</i> I'm on!</span>}
-      {light === "off" && <span className="Off"><i>💡</i> I'm off!</span>}
+    <button onClick={handleClick} className="LightSwitchButton">
+      {light === "on" && <span className="on"><i>💡</i> I'm on!</span>}
+      {light === "off" && <span className="off"><i>💡</i> I'm off!</span>}
     </button>
   );
 }
